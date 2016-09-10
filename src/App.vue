5b26212faef69dc83f047f627f989bf7ce6aa22b
Antonio <ant.ruggiero16@gmail.com>
2016-09-10 17:44:25 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <gagFeed :items="images"></gagFeed>
+  </div>
+</template>
+
+<script>
+import gagFeed from './components/Hello.vue'
+import request from 'request'
+import gag from 'node-9gag'
+export default {
+  data () {
+    return {
+      images:[]
+    }
+  },
+  components: {
+    gagFeed
+  }
+}
+console.log("popopopopopoop")
+/*request("http://infinigag.k3min.eu/hot",function(err,res,body){
+        console.log(body)
+})*/
+gag.section("hot",function(err,res){
+  console.log(err)
+  console.log(res)
+})
+</script>
+
+<style>
+</style>
---
+++ src/components/Hello.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="feed">
+    <div v-for="item in items">
+      <p>{{item.caption}}</p>
+      <img :src="item.images.normal" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {}
+  },
+  props:{
+  items:{
+    type:Array,
+    default:[]
+  }
+}   
+}
+</script>
